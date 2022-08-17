712e2605960641635764b0cc654eb451021b8bb6
Zach <thereezen24@gmail.com>
2022-08-17 02:16:40 -0400

    Added new Componet Nexpage to speed up work flow

---
+++ src/componets/nextpage/Nextpage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Nextpage.css';
+
+function Nextpage(){
+    return(
+        <body className="nexpage_body">
+            <div className="nextpage_content">
+                <p className="material-icons">expand_more</p>
+            </div>
+        </body>
+    );
+}
+
+export default Nextpage;
--- src/componets/projects/Projects.js
+++ src/componets/projects/Projects.js
@@ -1,11 +1,23 @@
 import React from "react";
 import './Projects.css';
+import Navbar from "../navbar/Navbar";
+import Nextpage from "../nextpage/Nextpage";
 
 
 function Projects(){
     return(
         <body className="projects_body">
-            
+            <div className="projects_container">
+                <div className="projects_nav">
+                    <Navbar />
+                </div>
+                <div className="projects_content">
+
+                </div>
+                <div className="projects_nextpage">
+                    <Nextpage />
+                </div>
+            </div>
         </body>
     );
 }
